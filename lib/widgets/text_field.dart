97862838ce3dbb64877233821d62f2234f8b011f
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2023-10-31 23:55:48 +0800

    Modularize custom text field widget

---
+++ lib/widgets/text_field.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String labelText;
+  final String hintText;
+
+  const CustomTextField({
+    Key? key,
+    required this.labelText,
+    required this.hintText,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(
+          vertical: 4.0), // Set your desired margin here
+      child: TextField(
+        decoration: InputDecoration(
+          labelText: labelText,
+          labelStyle: const TextStyle(fontSize: 15.0),
+          hintText: hintText,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(40.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomTextField1 extends StatelessWidget {
+  final String labelText;
+  final String hintText;
+  final double borderRadius;
+  final TextEditingController? controller; // Add controller parameter
+
+  const CustomTextField1({
+    Key? key,
+    required this.labelText,
+    required this.hintText,
+    this.borderRadius = 10.0,
+    this.controller, // Provide a default value of null for the controller
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 4.0),
+      child: TextField(
+        controller: controller, // Use the provided controller
+        decoration: InputDecoration(
+          labelText: labelText,
+          labelStyle: const TextStyle(fontSize: 15.0),
+          hintText: hintText,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(borderRadius),
+          ),
+        ),
+      ),
+    );
+  }
+}
