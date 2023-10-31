531c3fb14cd9529e93a1ffa882d069dfd45e4d08
John Heinrich <johnheinrich.fabros@tup.edu.ph>
2023-10-31 23:52:08 +0800

    Add app routes constant

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,8 @@
+const String loginRoute = '/login';
+const String dashboardRoute = '/dashboard';
+const String customerRoute = '/customer';
+const String driversRoute = '/drivers';
+const String productsRoute = '/items';
+const String accessoriesRoute = '/accessories';
+const String walkinRoute = '/walkin';
+const String transactionRoute = '/transactions';
