8ca71b7ee6e44e309402eccd661aebb40da204cc
Ian <kyutomana25@gmail.com>
2024-01-30 03:47:37 +0800

    Remove Delete in Approve

--- lib/views/newCustomers.page.dart
+++ lib/views/newCustomers.page.dart
@@ -41,15 +41,12 @@ class _NewCustomersState extends State<NewCustomers> {
             setState(() {
               customers = List<Map<String, dynamic>>.from(customerData);
             });
-          } else {
-            // Handle data format issues if needed
-          }
+          } else {}
         } else {
           print("Error: ${response.statusCode}");
         }
       }
     } catch (e) {
-      // Handle network request errors
       if (_mounted) {
         print("Error: $e");
       }
@@ -60,23 +57,22 @@ class _NewCustomersState extends State<NewCustomers> {
     await fetchCustomers();
   }
 
-  Future<void> deleteCustomer(String customerId) async {
-    try {
-      final response = await http.delete(
-        Uri.parse('https://lpg-api-06n8.onrender.com/api/v1/users/$customerId'),
-      );
+  // Future<void> deleteCustomer(String customerId) async {
+  //   try {
+  //     final response = await http.delete(
+  //       Uri.parse('https://lpg-api-06n8.onrender.com/api/v1/users/$customerId'),
+  //     );
 
-      if (response.statusCode == 200) {
-        // Successful deletion, you may want to update the UI accordingly
-        print('Customer deleted successfully');
-        await refreshData();
-      } else {
-        print('Error deleting customer: ${response.statusCode}');
-      }
-    } catch (e) {
-      print('Error: $e');
-    }
-  }
+  //     if (response.statusCode == 200) {
+  //       print('Customer deleted successfully');
+  //       await refreshData();
+  //     } else {
+  //       print('Error deleting customer: ${response.statusCode}');
+  //     }
+  //   } catch (e) {
+  //     print('Error: $e');
+  //   }
+  // }
 
   Future<void> updateVerificationStatus(String customerId) async {
     try {
@@ -130,7 +126,6 @@ class _NewCustomersState extends State<NewCustomers> {
           customers[customerIndex]['verified'] = true;
           customers[customerIndex]['discounted'] = true;
         }
-        // Update local data (customers list) after successful verification
         setState(() {});
         print(response.body);
 
@@ -152,7 +147,7 @@ class _NewCustomersState extends State<NewCustomers> {
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.white,
-        title: Text(
+        title: const Text(
           'Unverified Customers',
           style: TextStyle(color: Color(0xFF232937), fontSize: 24),
         ),
@@ -187,31 +182,26 @@ class _NewCustomersState extends State<NewCustomers> {
                                     "Contact Number: ${customer['contactNumber']}"),
                               ],
                             ),
-                            Spacer(),
+                            const Spacer(),
                             CircleAvatar(
                               radius: 30,
                               backgroundImage: NetworkImage(customer['image']),
                             ),
                           ],
                         ),
-                        Divider(),
-                        // Delete button
+                        const Divider(),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
                             TextButton(
                               onPressed: () async {
-                                // Call the updateVerificationStatus function with the customer ID
                                 await updateVerificationStatus(customer['_id']);
 
-                                // Show a SnackBar after approving
                                 ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(
+                                  const SnackBar(
                                     content:
                                         Text('Customer approved successfully'),
-                                    duration: Duration(
-                                        seconds:
-                                            2), // Adjust the duration as needed
+                                    duration: Duration(seconds: 2),
                                   ),
                                 );
                               },
@@ -219,41 +209,33 @@ class _NewCustomersState extends State<NewCustomers> {
                             ),
                             TextButton(
                               onPressed: () async {
-                                // Call the updateVerificationStatus function with the customer ID
                                 await updateVerificationStatusDiscount(
                                     customer['_id']);
 
-                                // Show a SnackBar after approving
                                 ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(
+                                  const SnackBar(
                                     content: Text(
                                         'Customer with discount approved successfully'),
-                                    duration: Duration(
-                                        seconds:
-                                            2), // Adjust the duration as needed
+                                    duration: Duration(seconds: 2),
                                   ),
                                 );
                               },
-                              child: Text('Approve with Discount'),
+                              child: const Text('Approve with Discount'),
                             ),
-                            TextButton(
-                              onPressed: () async {
-                                // Call the deleteCustomer function with the customer ID
-                                await deleteCustomer(customer['_id']);
+                            // TextButton(
+                            //   onPressed: () async {
+                            //     await deleteCustomer(customer['_id']);
 
-                                // Show a SnackBar after deletion
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(
-                                    content:
-                                        Text('Customer deleted successfully'),
-                                    duration: Duration(
-                                        seconds:
-                                            2), // Adjust the duration as needed
-                                  ),
-                                );
-                              },
-                              child: Text('Delete'),
-                            ),
+                            //     ScaffoldMessenger.of(context).showSnackBar(
+                            //       const SnackBar(
+                            //         content:
+                            //             Text('Customer deleted successfully'),
+                            //         duration: Duration(seconds: 2),
+                            //       ),
+                            //     );
+                            //   },
+                            //   child: const Text('Delete'),
+                            // ),
                           ],
                         ),
                       ],
@@ -274,24 +256,23 @@ class _NewCustomersState extends State<NewCustomers> {
       builder: (BuildContext context) {
         return SingleChildScrollView(
           child: Container(
-            padding: EdgeInsets.all(16.0),
+            padding: const EdgeInsets.all(16.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisSize: MainAxisSize.min,
               children: [
-                Text(
+                const Text(
                   'Customer Details',
                   style: TextStyle(
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                SizedBox(height: 10),
+                const SizedBox(height: 10),
                 Text('Name: ${customer['name']}'),
                 Text('Contact Number: ${customer['contactNumber']}'),
-
                 Text('Address: ${customer['address']}'),
-                Text('Discount ID:'),
+                const Text('Discount ID:'),
                 customer['discountIdImage'] != null
                     ? Image.network(
                         customer['discountIdImage'],
@@ -299,17 +280,15 @@ class _NewCustomersState extends State<NewCustomers> {
                         height: 300,
                         fit: BoxFit.cover,
                       )
-                    : Container(), // You can customize this placeholder
-
-                // Close button
-                SizedBox(height: 16),
+                    : Container(),
+                const SizedBox(height: 16),
                 Align(
                   alignment: Alignment.center,
                   child: ElevatedButton(
                     onPressed: () {
-                      Navigator.pop(context); // Close the modal
+                      Navigator.pop(context);
                     },
-                    child: Text('Close'),
+                    child: const Text('Close'),
                   ),
                 ),
               ],
